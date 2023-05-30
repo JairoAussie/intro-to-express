3836510f72c767d725aca32495b21782b6635e92
jairoaussie <jairo.bilbao@coderacademy.edu.au>
2023-05-30 20:19:30 +1000

    first step in refactoring

--- index.js
+++ index.js
@@ -1,19 +1,10 @@
-// import the express package
-const express = require('express')
-
-// create an instance of express
-const app = express()
-
-// define the port 
-const port = 3000
-
-app.get('/', (request, response) => {
-    response.send("Hello students!")
-})
-
+//import app and port
+const {app, port} = require("./server")
 
 // at the end of the file, create the listen method
 // to start listening to the requests from the client
+
+
 app.listen(port, ()=> {
     console.log(`Server running on port ${port}`)
 })
---
+++ server.js
@@ -0,0 +1,29 @@
+// import the express package
+const express = require('express')
+
+// create an instance of express
+const app = express()
+
+// define the port 
+const port = 3000
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+app.get('/', (request, response) => {
+
+    response.send(`<h1>Hello students!</h1>`)
+})
+
+app.post('/', (request, response) => {
+    // getting the content from the body of the request
+    const message = request.body.message
+    // send it back to the client
+    response.send(message)
+})
+
+app.delete('/', (request, response) => {
+    response.json({"message": "Deleted"})
+})
+
+module.exports = {app, port}
